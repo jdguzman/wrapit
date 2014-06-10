cb822dcad914bb31a9c37e38fd9668ba559aee92
JD Guzman <jdguzman@mac.com>
2014-06-10 12:57:52 -0500

    method_wrappable implemented

--- lib/wrapit/method_wrappable.rb
+++ lib/wrapit/method_wrappable.rb
@@ -4,6 +4,13 @@ module Wrapit::MethodWrappable
       private
 
       def method_wrappable(*args)
+        args.each do |method|
+          alias_method :"#{method}_naked", method
+
+          define_method method do
+            send("#{method}_naked").wrapped
+          end
+        end
       end
     end
   end
--- spec/wrapit/method_wrappable_spec.rb
+++ spec/wrapit/method_wrappable_spec.rb
@@ -7,6 +7,45 @@ describe Wrapit::MethodWrappable do
     destroy_class
   end
 
+  it "should add _naked version of method with method_wrappable" do
+    build_class
+    FooBar.module_eval { method_wrappable :to_s }
+    FooBar.new.respond_to?(:to_s_naked).should be_true
+    destroy_class
+  end
+
+  it "should create a wrapped version of method with method_wrappable" do
+    build_class
+    FooBar.module_eval { method_wrappable :to_s }
+    FooBar.new.to_s.should be_kind_of Present
+    destroy_class
+  end
+
+  it "wrapped method should return instance of Blank when nil returned" do
+    build_class
+    FooBar.module_eval { define_method :test_method do nil; end }
+    FooBar.module_eval { method_wrappable :test_method }
+    FooBar.new.test_method.should be_kind_of Blank
+    destroy_class
+  end
+
+  it "wrapped method should return instance of Present when anything other than nil is returned" do
+    build_class
+    FooBar.module_eval { define_method :test_method do "something"; end }
+    FooBar.module_eval { method_wrappable :test_method }
+    FooBar.new.test_method.should be_kind_of Present
+    destroy_class
+  end
+
+  it "wrapped method and _naked method should return same value" do
+    build_class
+    FooBar.module_eval { define_method :test_method do "something"; end }
+    FooBar.module_eval { method_wrappable :test_method }
+    foo_bar = FooBar.new
+    foo_bar.test_method.unwrap.should eq foo_bar.test_method_naked
+    destroy_class
+  end
+
   def build_class
     define_class 'FooBar', 'Object' do |klass|
       klass.send :include, Wrapit::MethodWrappable
